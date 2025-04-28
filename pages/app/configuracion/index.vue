8e1e349507b58f2000f5c659f523dd2b5320f60a
Rafael Acosta <rafa14494@gmail.com>
2025-04-27 20:47:34 -0700

    feat: add business account management components and update configuration page layout

--- pages/app/configuracion/index.vue
+++ pages/app/configuracion/index.vue
@@ -1,4 +1,12 @@
 <template>
+	<div class="grid grid-cols-2 gap-4">
+		<UiCard class="p-4">
+			<AccountBusiness />
+		</UiCard>
+		<UiCard class="col-start-1">
+			<AccountBusiness />
+		</UiCard>
+	</div>
 	<div>
 		<h2>Soy las configuraciones del negocio</h2>
 	</div>
